d4a0f4630119ae37228b8143b4b19c4ac9641697
Haochen M. Kotoi-Xie <haochenx@acm.org>
2024-10-23 19:47:51 +0900

    bug fix

--- src/webserver/index.tsx
+++ src/webserver/index.tsx
@@ -5,3 +5,5 @@ app.use("*", async (c, next) => {
   const response = await fetch(new Request("https://kxc.inc/404", c.req.raw));
   return new Response(response.body, response);
 });
+
+export default app;
